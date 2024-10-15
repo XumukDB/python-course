066c14e94cc4f397c2fe4eeb543ec07bce9205a3
XumukDB <daniil.belichenko.w@gmail.com>
2024-10-15 16:47:18 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_1.py
@@ -0,0 +1,7 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+index_missing = 4
+numbers[index_missing] = 0
+numbers[index_missing] = sum(numbers) / len(numbers)
+
+print("Измененный список:", numbers)
